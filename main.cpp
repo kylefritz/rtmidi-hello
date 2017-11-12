c4211ef87c2ae15077cb2400e6653684ca18d58c
Kyle Fritz <kfritz@uber.com>
2017-11-12 14:17:27 -0500

    Reading keys in a loop

--- main.cpp
+++ main.cpp
@@ -1,14 +1,50 @@
-#include <iostream>
+#include <chrono>
 #include <cstdlib>
+#include <iostream>
+#include <signal.h>
+#include <thread>
+
+// OSX unix support
+#include <unistd.h>
+
 #include "RtMidi.h"
+
+
+bool done;
+static void finish(int ignore){ done = true; }
 int main()
 {
-    RtMidiIn *midiIn = new RtMidiIn();
-    auto portCount = midiIn->getPortCount();
-    std::cout << "midi portCount=" << portCount << std::endl;
+    RtMidiIn *midiin = new RtMidiIn();
+    std::vector<unsigned char> message;
+    int nBytes, i;
+    double stamp;
+    // Check available ports.
+    unsigned int nPorts = midiin->getPortCount();
+    if ( nPorts == 0 ) {
+        std::cout << "No ports available!\n";
+        goto cleanup;
+    }
+    midiin->openPort( 0 );
+    // Don't ignore sysex, timing, or active sensing messages.
+    midiin->ignoreTypes( false, false, false );
+    // Install an interrupt handler function.
+    done = false;
+    (void) signal(SIGINT, finish);
+    // Periodically check input queue.
+    std::cout << "Reading MIDI from port ... quit with Ctrl-C.\n";
+    while ( !done ) {
+        stamp = midiin->getMessage( &message );
+        nBytes = message.size();
+        for ( i=0; i<nBytes; i++ )
+            std::cout << "Byte " << i << " = " << (int)message[i] << ", ";
+        if ( nBytes > 0 )
+            std::cout << "stamp = " << stamp << std::endl;
 
-    auto portName = midiIn->getPortName(0);
-    std::cout << "port_name=" << portName << std::endl;
-    
+        // Sleep for 10 milliseconds ...
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+    // Clean up
+    cleanup:
+    delete midiin;
     return 0;
 }
